2cfca1b9fec90afd7239a14fd640f3503af0f65b
DarkBumus <126115769+DarkBumus@users.noreply.github.com>
2025-11-25 23:43:00 +0100

    Change fetch source from tabs.json to packs_tabs.json

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("tabs.json")
+    fetch("packs_tabs.json")
         .then(response => response.json())
         .then(data => {
             const tabsContainer = document.getElementById("tabs");
