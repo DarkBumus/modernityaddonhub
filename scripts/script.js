fd0b785c2c88e77a25850bba2bac25da61cc785c
DarkBumus <126115769+DarkBumus@users.noreply.github.com>
2026-02-06 14:48:06 +0100

    Added PPA 1.7 Content
    Angelica Elytra
    Calm4.ogg for Record ^^
    Michael's Second Life
    Monocle Observer

--- scripts/script.js
+++ scripts/script.js
@@ -63,6 +63,7 @@ async function loadAllDownloadJSONs() {
         "jsons/downloadjsons/pprogrammerart1.7connected.json",
         "jsons/downloadjsons/pprogrammerart1.7crafting.json",
         "jsons/downloadjsons/pprogrammerart1.7fixes.json",
+        "jsons/downloadjsons/pprogrammerart1.7fun.json",
         "jsons/downloadjsons/pprogrammerart1.7randblocks.json",
         "jsons/downloadjsons/pprogrammerart1.7rotblocks.json",
         "jsons/downloadjsons/pprogrammerart1.7tweaks.json",
